92301e52e18ced43906cad180cffe160a63747d7
Charles Macanka <cmacanka@gmail.com>
2014-06-29 19:13:03 -0400

    Added a context getter to IPhlebotomistRepository.

--- Repositories/Interfaces/IPhlebotomistRepository.cs
+++ Repositories/Interfaces/IPhlebotomistRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Phlebotomist.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Phlebotomist.Repositories.Interfaces
 {
     public interface IPhlebotomistRepository
     {
+        PhlebotomistModelContainer Context { get; }
+
         void Save();
     }
 }
