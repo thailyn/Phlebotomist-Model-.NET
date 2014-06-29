3f7f224aaf9693e8682b5094699d94aee8d11611
Charles Macanka <cmacanka@gmail.com>
2014-06-29 18:17:32 -0400

    Moved BaseHP property into FamiliarTypeViewModel.

--- ViewModels/FamiliarTypeViewModel.cs
+++ ViewModels/FamiliarTypeViewModel.cs
@@ -291,6 +291,59 @@ namespace Phlebotomist.ViewModels
             }
         }
 
+        #region Stats
+        private int _baseStatTypeId = 1;
+        private int _maxStatTypeId = 2;
+        private int _peStatTypeId = 3;
+
+        private int _hpStatId = 1;
+        private int _atkStatId = 2;
+        private int _defStatId = 3;
+        private int _wisStatId = 4;
+        private int _agiStatId = 5;
+
+        public double BaseHP
+        {
+            get
+            {
+                var stats = from s in _familiarType.StatValues
+                            where s.StatTypeId == _baseStatTypeId
+                            where s.StatId == _hpStatId
+                            //where string.Equals(s.StatType.Name, "Base", StringComparison.CurrentCultureIgnoreCase)
+                            //where string.Equals(s.Stat.Name, "HP", StringComparison.CurrentCultureIgnoreCase)
+                            select s;
+
+                var stat = stats.ToList().FirstOrDefault();
+                return stat == null ? 0 : stat.StatValue;
+            }
+            set
+            {
+                var stats = from s in _familiarType.StatValues
+                            where s.StatTypeId == _baseStatTypeId
+                            where s.StatId == _hpStatId
+                            //where string.Equals(s.StatType.Name, "Base", StringComparison.CurrentCultureIgnoreCase)
+                            //where string.Equals(s.Stat.Name, "HP", StringComparison.CurrentCultureIgnoreCase)
+                            select s;
+
+                var stat = stats.ToList().FirstOrDefault();
+                if (stat == null)
+                {
+                    _familiarType.StatValues.Add(new FamiliarTypeStatValue
+                    {
+                        FamiliarTypeId = Id,
+                        StatTypeId = _baseStatTypeId,
+                        StatId = _hpStatId,
+                        StatValue = value
+                    });
+                }
+                else
+                {
+                    stat.StatValue = value;
+                }
+            }
+        }
+        #endregion
+
         public FamiliarTypeViewModel(FamiliarType model, IPhlebotomistRepository phlebotomistRepository)
         {
             _familiarType = model;
