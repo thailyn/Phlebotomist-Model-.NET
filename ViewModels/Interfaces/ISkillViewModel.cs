0cb91cc019991b8626c737ac1ee9b540bd18a570
Charles Macanka <cmacanka@gmail.com>
2014-06-29 20:27:29 -0400

    Added basic version of SkillViewModel.

---
+++ ViewModels/Interfaces/ISkillViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Phlebotomist.Model;
+using Phlebotomist.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phlebotomist.ViewModels.Interfaces
+{
+    public interface ISkillViewModel
+    {
+        #region Properties
+        #region Model
+        Skill Skill { get; }
+        #endregion
+
+        #region Binding
+        long Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        //*********************************//
+        #endregion
+        #endregion
+
+        #region Utility
+        /// <summary>
+        /// Gets or sets the repository for saving to the Phlebotomist database.
+        /// </summary>
+        IPhlebotomistRepository PhlebotomistRepository { get; set; }
+        #endregion
+
+        #region Accessors
+        #endregion
+
+        #region Mutators
+        #endregion
+
+        /// <summary>
+        /// Saves changes to the underlying model object to the database.
+        /// </summary>
+        /// <returns>True iff the save was successful.</returns>
+        bool Save();
+    }
+}
---
+++ ViewModels/SkillViewModel.cs
@@ -0,0 +1,127 @@
+﻿using Phlebotomist.Model;
+using Phlebotomist.Repositories.Interfaces;
+using Phlebotomist.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phlebotomist.ViewModels
+{
+    public class SkillViewModel : INotifyPropertyChanged, ISkillViewModel
+    {
+        #region Utility
+        /// <summary>
+        /// Gets or sets the repository for saving to the Phlebotomist database.
+        /// </summary>
+        public IPhlebotomistRepository PhlebotomistRepository { get; set; }
+        #endregion
+
+        private Skill _skill;
+        public Skill Skill
+        {
+            get
+            {
+                return _skill;
+            }
+            protected set
+            {
+                if (value != _skill)
+                {
+                    _skill = value;
+                    OnPropertyChanged("FamiliarType");
+                }
+            }
+        }
+
+        public long Id
+        {
+            get
+            {
+                return _skill.Id;
+            }
+            set
+            {
+                if (value != _skill.Id)
+                {
+                    _skill.Id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _skill.Name;
+            }
+            set
+            {
+                if (value != _skill.Name)
+                {
+                    _skill.Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _skill.Description;
+            }
+            set
+            {
+                if (value != _skill.Description)
+                {
+                    _skill.Description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        public SkillViewModel(Skill model, IPhlebotomistRepository phlebotomistRepository)
+        {
+            _skill = model;
+            PhlebotomistRepository = phlebotomistRepository;
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                if (Skill.Id < 0)
+                {
+                    var repository = PhlebotomistRepository as IPhlebotomistRepository;
+                    repository.Context.Skills.Add(Skill);
+                }
+
+                PhlebotomistRepository.Save();
+            }
+            catch (Exception)
+            {
+                // log info
+                throw;
+            }
+
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
